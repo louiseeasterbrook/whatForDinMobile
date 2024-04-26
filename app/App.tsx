cd8f9422f6878c1a96a059e5784e770d86e194f0
louise <louise.easterbrook@gmail.com>
2024-04-26 23:35:21 +1000

    android splash screen update

--- app/App.tsx
+++ app/App.tsx
@@ -9,21 +9,20 @@ import React, {useEffect} from 'react';
 
 import {Environment} from './models/environment';
 import {MainStore, RootStoreProvider} from './store/mainStore';
-import {useColorScheme} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
-import {
-  MD3LightTheme as DefaultTheme,
-  MD3DarkTheme as DarkTheme,
-  PaperProvider,
-} from 'react-native-paper';
+import {PaperProvider} from 'react-native-paper';
 import {RootNavigator} from './navigaton/root.navigator';
 
 function App(): React.JSX.Element {
   const rootStore = MainStore.create({});
   const environment = Environment.getInstance();
 
-  //TODO: move to own setup function in diff file
   useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 500);
+
     (async () => {
       await environment.setup();
     })();
