50d2f7752a804b7f0a0a38f28d8aa53cc3e0000b
louise <louise.easterbrook@gmail.com>
2024-04-27 12:54:45 +1000

    update status bar colour

--- app/components/BaseScreen.component.tsx
+++ app/components/BaseScreen.component.tsx
@@ -1,5 +1,11 @@
-import {Platform, StatusBar, StyleSheet, View} from 'react-native';
-import {useSafeAreaInsets, SafeAreaView} from 'react-native-safe-area-context';
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+  SafeAreaView,
+} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 type BaseScreenProps = {
   children: any;
@@ -7,14 +13,25 @@ type BaseScreenProps = {
   useSafeArea?: boolean;
 };
 
-//TODO: status bar colour
 export const BaseScreen = ({
   children,
   statusBarColour = '#FFFFFF',
   useSafeArea = false,
 }: BaseScreenProps) => {
+  const insets = useSafeAreaInsets();
+  const statusBarHeightAndroid = StatusBar.currentHeight + 14;
+
+  const STATUSBAR_HEIGHT =
+    Platform.OS === 'ios' ? insets.top : statusBarHeightAndroid;
+
   return (
     <View style={styles.screen}>
+      <View style={{height: STATUSBAR_HEIGHT}}>
+        <StatusBar
+          backgroundColor={statusBarColour}
+          translucent
+          barStyle="dark-content"></StatusBar>
+      </View>
       {useSafeArea ? (
         <SafeAreaView style={styles.screen}>{children}</SafeAreaView>
       ) : (
