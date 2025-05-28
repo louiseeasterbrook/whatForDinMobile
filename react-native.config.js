fb729c0afe9b44b483d2bff2935d0426376cfbdd
louise <louise.easterbrook@gmail.com>
2025-05-28 19:10:35 +1000

    fix up font on ios

--- react-native.config.js
+++ react-native.config.js
@@ -6,5 +6,5 @@ module.exports = {
       },
     },
   },
-  assets: ['./node_modules/react-native-vector-icons/Fonts'],
+  assets: ['./node_modules/react-native-vector-icons/Fonts', './assets/fonts'],
 };
