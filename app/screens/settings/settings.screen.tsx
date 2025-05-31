09f660457240025923924c5cd6c2df156dd7ab55
louise <louise.easterbrook@gmail.com>
2025-05-31 16:07:31 +1000

    settings ui uplift

--- app/screens/settings/settings.screen.tsx
+++ app/screens/settings/settings.screen.tsx
@@ -1,5 +1,5 @@
 import React, {ReactNode} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, SafeAreaView} from 'react-native';
 import {BaseScreen} from '../../components/BaseScreen.component';
 import {Avatar, IconButton} from 'react-native-paper';
 import {useStores} from '../../store/mainStore';
@@ -27,41 +27,73 @@ export const SettingsScreen = ({navigation}): ReactNode => {
 
   return (
     <BaseScreen noStatusBar={false}>
-      <View style={styles.fullScreenContainer}>
-        <View>
+      <SafeAreaView style={{flex: 1}}>
+        <View style={styles.fullScreenContainer}>
+          {/* Header */}
           <View style={styles.headerContainer}>
+            <IconButton
+              icon="arrow-left"
+              size={24}
+              onPress={() => navigation.goBack()}
+            />
+            <View style={{flex: 1}} />
             <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <IconButton
-                icon="arrow-left"
-                size={24}
-                onPress={() => navigation.goBack()}
-              />
-              <Avatar.Icon size={44} icon="account" />
               <PrimaryText addedStyles={styles.name} text={userStore.name} />
+              <Avatar.Icon
+                size={40}
+                icon="account"
+                style={{backgroundColor: '#f2f2f2', marginLeft: 8}}
+              />
             </View>
-            <IconButton icon="logout" size={20} onPress={logout} />
           </View>
-
+          {/* Divider */}
+          <View
+            style={{
+              height: 1,
+              backgroundColor: '#ececec',
+              marginHorizontal: 20,
+              marginBottom: 10,
+            }}
+          />
+          {/* Settings Section */}
           <View style={styles.sidePadding}>
+            <PrimaryText
+              text="Settings"
+              size={13}
+              textColour="#888"
+              addedStyles={{marginBottom: 8, marginLeft: 2}}
+            />
             <SettingsRow title="Recipe tags" onPress={navToRecipeTag} topRow />
             <SettingsRow
               title="Search users"
               onPress={() => navigation.navigate('SearchUsers')}
               bottomRow
             />
-            {/* <SettingsRow
-              title="Meal Picker"
-              onPress={navToMealPicker}
-              bottomRow></SettingsRow> */}
           </View>
-        </View>
-        <View>
+          {/* Spacer */}
+          <View style={{flex: 1}} />
+          {/* Logout Button */}
+          <View style={{alignItems: 'center', marginBottom: 16}}>
+            <IconButton
+              icon="logout"
+              size={24}
+              onPress={logout}
+              style={{backgroundColor: '#f8d7da', borderRadius: 24}}
+            />
+            <PrimaryText
+              text="Logout"
+              size={13}
+              textColour="#c0392b"
+              addedStyles={{marginTop: -8, marginBottom: 8}}
+            />
+          </View>
+          {/* Version Info */}
           <PrimaryText
             addedStyles={styles.version}
             text={`Version ${DeviceInfo.getVersion()}`}
           />
         </View>
-      </View>
+      </SafeAreaView>
     </BaseScreen>
   );
 };
@@ -70,12 +102,10 @@ const styles = StyleSheet.create({
   headerContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 10,
-    paddingVertical: 12,
+    paddingHorizontal: 16,
+    paddingTop: 8,
+    paddingBottom: 8,
     backgroundColor: 'white',
-    ...SHADOW_BASE,
-    marginBottom: 12,
   },
   nameContainer: {
     flexDirection: 'row',
@@ -93,7 +123,9 @@ const styles = StyleSheet.create({
   },
   version: {
     textAlign: 'center',
-    paddingBottom: 20,
+    paddingBottom: 10,
+    color: '#aaa',
+    fontSize: 12,
   },
   sidePadding: {
     marginHorizontal: 20,
